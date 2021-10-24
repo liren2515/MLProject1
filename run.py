c618a6702ce4ee2139e21b344d4ae454c0606b35
Ren Li <renli@iccluster132.iccluster.epfl.ch>
2021-10-24 11:29:33 +0200

    new run

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import os, sys
 import time
 import numpy as np
-from lib.data import load_data, statistic_invalid, standardize, save_data, cross_validation, build_poly, add_bias
+from lib.data import load_data, statistic_invalid, standardize, save_data, cross_validation, build_poly, build_sin, build_poly_with_interation, build_poly_with_interation_3, build_sin_2, build_poly_power, add_bias
 from lib import config
 from lib import method
 from lib.plots import plot_train_test
@@ -26,18 +26,78 @@ data_DER_raw = data_DER_raw[:,config.valid_DER_idx]
 data_PRI_raw = data_PRI_raw[:,config.valid_PRI_idx] # 8-D
 
 data_DER_raw_test = data_DER_raw_test[:,config.valid_DER_idx]
-
+data_PRI_raw_test = data_PRI_raw_test[:,config.valid_PRI_idx]
 # polynomial augmentation
 if config.poly:
+    '''
     data_DER_raw = build_poly(data_DER_raw, degree=config.poly_degree)
     data_DER_raw_test = build_poly(data_DER_raw_test, degree=config.poly_degree)
 
+    data_PRI_raw = build_poly(data_PRI_raw, degree=config.poly_degree)
+    data_PRI_raw_test = build_poly(data_PRI_raw_test, degree=config.poly_degree)
+    '''
+
+    
+    data_DER_raw_sin2 = build_sin_2(data_DER_raw)
+    data_DER_raw_test_sin2 = build_sin_2(data_DER_raw_test)
+
+    data_PRI_raw_sin2 = build_sin_2(data_PRI_raw)
+    data_PRI_raw_test_sin2 = build_sin_2(data_PRI_raw_test)
+
+    data_DER_raw_power = build_poly_power(data_DER_raw, degree=config.poly_degree)
+    data_DER_raw_test_power = build_poly_power(data_DER_raw_test, degree=config.poly_degree)
+
+    data_PRI_raw_power = build_poly_power(data_PRI_raw, degree=config.poly_degree)
+    data_PRI_raw_test_power = build_poly_power(data_PRI_raw_test, degree=config.poly_degree)
+    
+    '''
+    data_DER_raw = build_poly_with_interation(data_DER_raw)
+    data_DER_raw_test = build_poly_with_interation(data_DER_raw_test)
+
+    data_PRI_raw = build_poly_with_interation(data_PRI_raw)
+    data_PRI_raw_test = build_poly_with_interation(data_PRI_raw_test)
+    '''
+    data_DER_raw = build_poly_with_interation_3(data_DER_raw)
+    data_DER_raw_test = build_poly_with_interation_3(data_DER_raw_test)
+
+    data_PRI_raw = build_poly_with_interation_3(data_PRI_raw)
+    data_PRI_raw_test = build_poly_with_interation_3(data_PRI_raw_test)
+    
+    data_DER_raw = np.concatenate((data_DER_raw, data_DER_raw_sin2, data_DER_raw_power), axis=-1)
+    data_DER_raw_test = np.concatenate((data_DER_raw_test, data_DER_raw_test_sin2, data_DER_raw_test_power), axis=-1)
+    data_PRI_raw = np.concatenate((data_PRI_raw, data_PRI_raw_sin2, data_PRI_raw_power), axis=-1)
+    data_PRI_raw_test = np.concatenate((data_PRI_raw_test, data_PRI_raw_test_sin2, data_PRI_raw_test_power), axis=-1)
+    
+    
+    
+
+print(data_DER_raw.shape, data_PRI_raw.shape)
+#sys.exit()
 data_DER, mean_DER, std_DER = standardize(data_DER_raw)
 data_DER_test = (data_DER_raw_test - mean_DER) / std_DER
 data_DER = add_bias(data_DER)
 data_DER_test = add_bias(data_DER_test)
 
+data_PRI, mean_PRI, std_PRI = standardize(data_PRI_raw)
+data_PRI_test = (data_PRI_raw_test - mean_PRI) / std_PRI
+data_PRI = add_bias(data_PRI)
+data_PRI_test = add_bias(data_PRI_test)
+
+data_DER = np.concatenate((data_DER, data_PRI), axis=-1)
+data_DER_test = np.concatenate((data_DER_test, data_PRI_test), axis=-1)
+
+'''
+print(np.max(mean_DER), np.min(mean_DER))
+print(np.max(std_DER), np.min(std_DER))
+print(mean_DER)
+print(std_DER)
+print(np.max(data_DER, axis=0))
+print(np.min(data_DER, axis=0))
+sys.exit()
+'''
+
 function_name = config.methods[config.method_idx]
+save_sufix = 'mix'
 
 if function_name == 'GD':
     function = method.least_squares_GD
@@ -99,14 +159,15 @@ elif function_name == 'ridge':
     function = method.ridge_regression
     y = prediction
     tx = data_DER
-    lambdas = np.logspace(-5, -2, 30) #degree 1/2
+    #lambdas = np.logspace(-5, -2, 30) #degree 1/2
     #lambdas = np.logspace(-6, -3, 15) #degree 3
     #lambdas = np.logspace(-15, -10, 30) #degree 7
     #lambdas = np.logspace(-16, -12, 30) #degree 10
     #lambdas = np.logspace(-16, -12, 30) #degree 15
+    lambdas = np.logspace(-14, -9, 15) #mix degree 15
 
     num_samples = len(y)
-    ratio = 0.1
+    ratio = 0.2
     seed = 42
     train_sets, val_sets = cross_validation(num_samples, ratio=ratio, seed=seed)
     record_acc_train = []   
@@ -143,7 +204,7 @@ elif function_name == 'ridge':
         os.makedirs(save_folder)
     
     if config.poly:
-        fig_save_path = os.path.join(save_folder, 'ridge_regression_degree_%d.png'%config.poly_degree)
+        fig_save_path = os.path.join(save_folder, 'ridge_regression_degree_%d_%s.png'%(config.poly_degree,save_sufix))
     else:
         fig_save_path = os.path.join(save_folder, 'ridge_regression.png')
     plot_train_test(record_acc_train, record_acc_val, lambdas, fig_save_path)
@@ -156,7 +217,7 @@ elif function_name == 'ridge':
     prediction_test[prediction_test>=0] = 1
 
     if config.poly:
-        csv_save_path = os.path.join(save_folder, 'ridge_regression_degree_%d.csv'%config.poly_degree)
+        csv_save_path = os.path.join(save_folder, 'ridge_regression_degree_%d_%s.csv'%(config.poly_degree,save_sufix))
     else:
         csv_save_path = os.path.join(save_folder, 'ridge_regression.csv')
     save_data(prediction_test, csv_save_path)
@@ -165,7 +226,68 @@ elif function_name == 'logistic':
     function = method.logistic_regression
 
 elif function_name == 'reg_logistic':
+    print('Regularized Logistic Regression!')
     function = method.reg_logistic_regression
+
+    y = prediction
+    tx = data_DER
+    lambdas = np.logspace(-18, -2, 15)
+    max_iters = 10000
+    gamma = 0.1
+    np.random.seed(42)
+    initial_w = np.zeros(tx.shape[-1])#np.random.randn(tx.shape[-1])
+    print('initial_w: ', initial_w)
+    
+    num_samples = len(y)
+    ratio = 0.1
+    seed = 42
+    train_sets, val_sets = cross_validation(num_samples, ratio=ratio, seed=seed)
+    record_acc_train = []   
+    record_acc_val = []
+    record_w = []
+    for ind, lambda_ in enumerate(lambdas):
+        acc_train, acc_val = [], []
+        for train_set, val_set in zip(train_sets, val_sets):
+            y_train, y_val = y[train_set], y[val_set]
+            tx_train, tx_val = tx[train_set], tx[val_set]
+            loss, w = function(y_train, tx_train, lambda_,  initial_w, max_iters, gamma)
+            prediction_train = method.sigmoid(tx_train.dot(w))
+            prediction_train[prediction_train<0.5] = -1
+            prediction_train[prediction_train>=0.5] = 1
+            prediction_val = method.sigmoid(tx_val.dot(w))
+            prediction_val[prediction_val<0.5] = -1
+            prediction_val[prediction_val>=0.5] = 1
+
+            acc_tr = (prediction_train == y_train).astype(float).sum()/len(y_train)
+            acc_v = (prediction_val == y_val).astype(float).sum()/len(y_val)
+            acc_train.append(acc_tr)
+            acc_val.append(acc_v)
+        record_acc_train.append(sum(acc_train)/len(acc_train))
+        record_acc_val.append(sum(acc_val)/len(acc_val))
+
+    save_folder = 'results/reg_logistic' if not config.data_balance else 'results/reg_logistic_balance'
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+    
+    if config.poly:
+        fig_save_path = os.path.join(save_folder, 'reg_logistic_degree_%d_%s.png'%(config.poly_degree, save_sufix))
+    else:
+        fig_save_path = os.path.join(save_folder, 'reg_logistic.png')
+    plot_train_test(record_acc_train, record_acc_val, lambdas, fig_save_path)
+    
+    idx_max = np.argmax(np.array(record_acc_val))
+    lambda_ = lambdas[idx_max]
+    
+    _, w = function(y, tx, lambda_, initial_w, max_iters, gamma)
+    prediction_test = method.sigmoid(data_DER_test.dot(w))
+    prediction_test[prediction_test<0.5] = -1
+    prediction_test[prediction_test>=0.5] = 1
+
+    if config.poly:
+        csv_save_path = os.path.join(save_folder, 'reg_logistic_degree_%d_%s.csv'%(config.poly_degree, save_sufix))
+    else:
+        csv_save_path = os.path.join(save_folder, 'reg_logistic.csv')
+    save_data(prediction_test, csv_save_path)
 
 else:
     print('Do Nothing!')
